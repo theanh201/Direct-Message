4b24bc9bfbd0dcc013e98dc459956649a9afc4f3
Admin <theanh20000000@gmail.com>
2024-05-14 15:22:14 +0700

    Update search.js add popup menu

--- src/screens/search/search.js
+++ src/screens/search/search.js
@@ -1,16 +1,41 @@
-import { View, Text, TouchableOpacity, TextInput, StyleSheet,FlatList, Image, ActivityIndicator } from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, StyleSheet,FlatList, Image, ActivityIndicator, Modal } from 'react-native'
 import React, { useState } from 'react'
 import AntDesign from 'react-native-vector-icons/AntDesign'
 import Colors from '../../asset/styles/color'
 import { ValidateEmail, DOMAIN, TOKEN } from '../../config/const'
 import axios from 'axios'
+import { Button, Overlay, Icon } from '@rneui/themed';
 
 export default function SearchScreen({navigation}) {
   const [searchString, setSearchString] = useState("");
   const [loading, setLoading] = useState(false);
   const [searchResult, setSearchResult] = useState([]);
   const [page, setPage] = useState(0);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [background, setBackground] = useState("");
 
+  const modal = (name, email, background, avatar) =>{
+    setModalVisible(true);
+    setName(name);
+    setAvatar(avatar);
+    setBackground(background);
+    setEmail(email);
+  };
+  const item = ({item}) => (
+    <View style={styles.friendContainer}>
+      <Image style={styles.img} source={{uri:`${DOMAIN}/get-avatar/${TOKEN.GetToken()}/${item.avatar}`}}/>
+      <View style={styles.info}>
+        <Text style={styles.name}>{item.name}</Text>
+        <Text style={styles.dob}>{item.email}</Text>
+      </View>
+      <TouchableOpacity onPress={()=> modal(item.name, item.email, item.background, item.avatar)}>
+        <AntDesign name="plus" size={25} color={Colors._black}/>
+      </TouchableOpacity>
+    </View>
+  );
   const searchUser = async (searchString, page) => {
     if(page<0){
       return;
@@ -63,6 +88,30 @@ export default function SearchScreen({navigation}) {
           <AntDesign name='search1' size={20} color={Colors._white} style={{marginLeft:5}}/>
         </TouchableOpacity>
       </View>
+      <Modal
+        style={{alignItems:'center'}}
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}>
+        <View style={{alignItems:"center", justifyContent:"center", flex:1}}>
+          <View style={{backgroundColor:"white", borderWidth:1, borderRadius:10, alignContent:"center", width:"90%"}}>
+            <Image style={{height:"30%", width:"100%", borderRadius:10}} source={{uri:`${DOMAIN}/get-background/${TOKEN.GetToken()}/${background}`}}/>
+            <View style={{alignItems:'center', top:-50}}>
+              <Image style={{height:100, width:100, borderColor:"gray", borderWidth:2, borderRadius:360}} source={{uri:`${DOMAIN}/get-avatar/${TOKEN.GetToken()}/${avatar}`}}/>
+              <Text style={{fontSize:25}}>{name}</Text>
+              <Text>Email: {email}</Text>
+            </View>
+            <View style={{bottom:5, paddingHorizontal:5, position:"absolute", width:"100%"}}>
+              <TouchableOpacity onPress={() => {setModalVisible(false)}} style={{backgroundColor:"green", borderRadius:5, alignItems:"center", marginBottom:5}}>
+                <Text style={{fontSize:20, padding:5}}>AddFriend</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {setModalVisible(false)}} style={{backgroundColor:"red", borderRadius:5, alignItems:"center"}}>
+                <Text style={{fontSize:20, padding:5}}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
       {/* if loading */}
       {loading ? (<ActivityIndicator size="large" color="#0000ff" />) : 
       // if result not empty
@@ -70,21 +119,8 @@ export default function SearchScreen({navigation}) {
         <FlatList
           style={{height:"90%"}}
           data={searchResult}
-          renderItem={({item})=>(
-            <View style={styles.friendContainer}>
-              <Image style={styles.img} source={{uri:`${DOMAIN}/get-avatar/${TOKEN.GetToken()}/${item.avatar}`}}/>
-              <View style={styles.info}>
-                <Text style={styles.name}>{item.name}</Text>
-                <Text style={styles.dob}>{item.email}</Text>
-              </View>
-              <TouchableOpacity>
-                <AntDesign name="plus" size={25} color={Colors._black}/>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <AntDesign name="question" size={25} color={Colors._black}/>
-              </TouchableOpacity>
-            </View>
-          )}
+          renderItem={item}
+          keyExtractor={item => item.email}
           ListFooterComponent={() =>{
             if (searchResult.length > 9){
               return(
